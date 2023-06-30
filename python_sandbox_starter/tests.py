fe5d5e279c7bb71807b6d37c13bd9df58a0be88d
NatashaCamille <nnalubega@outlook.com>
2023-07-01 00:56:51 +0300

    Working on some classes

--- python_sandbox_starter/tests.py
+++ python_sandbox_starter/tests.py
@@ -3,9 +3,13 @@ class Customer:
         self.name = name
         self.age = age
         self.membership_type = membership_type
-        print("customer created")
+    
+    def update_membership(self, new_membrship):
+        self.membership_type = new_membrship
 
-customers = [Customer("Camille", 30, "Gold"), Customer("Natasha", 29, "Platinum")]
+customers = [Customer("Camille", 30, "Gold"),
+             Customer("Natasha", 29, "Platinum")]
 
-print(customers[1].name)
+print(customers[1].membership_type)
+customers[1].update_membership("Gold")
  
