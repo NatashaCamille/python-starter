8270d52113effdc2b8f84ee148f5efb34ccd2222
NatashaCamille <nnalubega@outlook.com>
2023-07-01 01:08:42 +0300

    Testing static methods

--- python_sandbox_starter/tests.py
+++ python_sandbox_starter/tests.py
@@ -5,11 +5,19 @@ class Customer:
         self.membership_type = membership_type
     
     def update_membership(self, new_membrship):
+        print("Calculating costs")
         self.membership_type = new_membrship
+
+    #Static method
+    def read_customer():
+        print("Reading customer from DB")
 
 customers = [Customer("Camille", 30, "Gold"),
              Customer("Natasha", 29, "Platinum")]
 
 print(customers[1].membership_type)
 customers[1].update_membership("Gold")
+print(customers[1].membership_type)
+
+Customer.read_customer()
  
